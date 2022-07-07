96dd0074d4cde3e59a2c340b226fc75f9ed37d36
lisonge <i@songe.li>
2022-07-07 17:32:30 +0800

    fix: del npx

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  // splitting: false,
   sourcemap: true,
   clean: true,
   dts: true,
   outDir: 'dist',
   format: ['cjs', 'esm'],
-  onSuccess: 'npx tsc --emitDeclarationOnly --declaration',
+  onSuccess: 'tsc --emitDeclarationOnly --declaration',
 });

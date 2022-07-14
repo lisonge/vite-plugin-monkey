31ed164cd3706ba719cdc0cf2420f28673cbaed1
lisonge <i@songe.li>
2022-07-14 21:53:33 +0800

    feat: support vite@^3

--- src/index.ts
+++ src/index.ts
@@ -461,14 +461,19 @@ export default (pluginOption: MonkeyOption): Plugin => {
           /__HMR_PROTOCOL__/g,
           `(__HMR_PROTOCOL__ || ((()=>{const u = new URL(import.meta['url'], location.origin);return u.protocol === 'https:' ? 'wss' : 'ws'})()))`
         );
+
+        // vite@v3 not need, see https://github.com/vitejs/vite/blob/v3.0.0/packages/vite/src/client/client.ts#L26
         code = code.replace(
           /__HMR_HOSTNAME__/g,
           `(__HMR_HOSTNAME__ || ((()=>{const u = new URL(import.meta['url'], location.origin);return u.hostname})()))`
         );
+
+        // vite@v3 not need, see https://github.com/vitejs/vite/blob/v3.0.0/packages/vite/src/client/client.ts#L29
         code = code.replace(
           /__HMR_PORT__/g,
           `(__HMR_PORT__ || ((()=>{const u = new URL(import.meta['url'], location.origin);return u.prort})()))`
         );
+
         code = code.replace(
           /__BASE__/g,
           `((()=>{const b = __BASE__; const u = new URL(import.meta['url'], location.origin); return b !== '/' ? b : (u.origin+'/');})())`

d54e363a06e984cb6b0e18178fa6ce09a1a6c59d
lisonge <i@songe.li>
2022-08-14 17:39:57 +0800

    perf: locale type example

--- packages/vite-plugin-monkey/src/node/userscript/common.ts
+++ packages/vite-plugin-monkey/src/node/userscript/common.ts
@@ -1,8 +1,14 @@
 export type IArray<T = unknown> = T | T[];
 
 /**
+ * key is language code or ''
  * @see https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
- * key is language code
+ * @example
+ * {
+ *   '':'default_name',
+ *   'zh-CN':'名字',
+ *   ja: '名前'
+ * }
  */
 export type LocaleType<T = unknown> = Record<string, T>;
 

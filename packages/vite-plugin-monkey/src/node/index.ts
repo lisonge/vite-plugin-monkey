df729411bf2ac19fad71fdcfd46cd672d2f09ff2
lisonge <i@songe.li>
2022-08-17 11:10:27 +0800

    docs: add comment and type hint comment

--- packages/vite-plugin-monkey/src/node/index.ts
+++ packages/vite-plugin-monkey/src/node/index.ts
@@ -68,7 +68,8 @@ export type MonkeyOption = {
   clientAlias?: string;
   server?: {
     /**
-     * auto open *.user.js in default browser when userscript comment change or vite server first start
+     * auto open *.user.js in default browser when userscript comment change or vite server first start.
+     * if you don't want to open when vite server first start, just want to open when userscript comment change, you should set viteConfig.server.open=false
      * @default true
      */
     open?: boolean;
@@ -82,6 +83,34 @@ export type MonkeyOption = {
     /**
      * mount GM_api to unsafeWindow, not recommend it, you should use GM_api by ESM import
      * @default false
+     * @example
+     * // if set true, you can export all from vite-plugin-monkey/dist/client to global
+     * // vite-env.d.ts
+     * type MonkeyWindow = import('vite-plugin-monkey/dist/client').MonkeyWindow;
+     * declare const unsafeWindow: MonkeyWindow['unsafeWindow'];
+     * declare const GM_addStyle: MonkeyWindow['GM_addStyle'];
+     * declare const GM_addElement: MonkeyWindow['GM_addElement'];
+     * declare const GM_deleteValue: MonkeyWindow['GM_deleteValue'];
+     * declare const GM_listValues: MonkeyWindow['GM_listValues'];
+     * declare const GM_addValueChangeListener: MonkeyWindow['GM_addValueChangeListener'];
+     * declare const GM_removeValueChangeListener: MonkeyWindow['GM_removeValueChangeListener'];
+     * declare const GM_setValue: MonkeyWindow['GM_setValue'];
+     * declare const GM_getValue: MonkeyWindow['GM_getValue'];
+     * declare const GM_log: MonkeyWindow['GM_log'];
+     * declare const GM_getResourceText: MonkeyWindow['GM_getResourceText'];
+     * declare const GM_getResourceURL: MonkeyWindow['GM_getResourceURL'];
+     * declare const GM_registerMenuCommand: MonkeyWindow['GM_registerMenuCommand'];
+     * declare const GM_unregisterMenuCommand: MonkeyWindow['GM_unregisterMenuCommand'];
+     * declare const GM_openInTab: MonkeyWindow['GM_openInTab'];
+     * declare const GM_xmlhttpRequest: MonkeyWindow['GM_xmlhttpRequest'];
+     * declare const GM_download: MonkeyWindow['GM_download'];
+     * declare const GM_getTab: MonkeyWindow['GM_getTab'];
+     * declare const GM_saveTab: MonkeyWindow['GM_saveTab'];
+     * declare const GM_getTabs: MonkeyWindow['GM_getTabs'];
+     * declare const GM_notification: MonkeyWindow['GM_notification'];
+     * declare const GM_setClipboard: MonkeyWindow['GM_setClipboard'];
+     * declare const GM_info: MonkeyWindow['GM_info'];
+     * declare const GM_cookie: MonkeyWindow['GM_cookie'];
      */
     mountGmApi?: boolean;
   };
@@ -132,7 +161,7 @@ export type MonkeyOption = {
     autoGrant?: boolean;
 
     /**
-     * check all require urls for availability, http code is 2xx
+     * check all require urls for availability, just http code is 2xx, never check http.body
      * @default false
      */
     checkCDN?: boolean;

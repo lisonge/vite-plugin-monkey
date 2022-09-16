1bce04f35a2224419fcc815cf9caffe5192dbea8
lisonge <i@songe.li>
2022-09-16 22:26:44 +0800

    docs: add comment  for util.encodeFn

--- packages/vite-plugin-monkey/src/node/util.ts
+++ packages/vite-plugin-monkey/src/node/util.ts
@@ -1,5 +1,30 @@
 import { transformWithEsbuild } from 'vite';
-// eslint-disable-next-line
+/*eslint @typescript-eslint/no-explicit-any: "off"*/
+
+/**
+ * transform function and parameter to iife function string, then transform this string to dataUrl
+ *
+ * this tool function is used to make umd-cdn and iife-cdn work normally, such as element-plus and vue
+ *
+ * @example
+ * // this example comes form playground/ex-vue-demi/vite.config.ts
+ * const externalGlobals = [
+ *   [
+ *     'vue',
+ *     cdn
+ *       .jsdelivr('Vue', 'dist/vue.global.prod.js')
+ *       .concat('https://unpkg.com/vue-demi@latest/lib/index.iife.js')
+ *       .concat(
+ *         await util.encodeFn(() => {
+ *           // \@ts-ignore
+ *           window.Vue = Vue; // work with element-plus
+ *         }, []),
+ *       ),
+ *   ],
+ *   ['pinia', cdn.jsdelivr('Pinia', 'dist/pinia.iife.prod.js')],
+ *   ['element-plus', cdn.jsdelivr('ElementPlus', 'dist/index.full.min.js')],
+ * ];
+ */
 export const encodeFn = async <T extends (...args: any[]) => any>(
   fn: T,
   args: Parameters<T>,

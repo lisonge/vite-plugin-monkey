394032ee69d566a6d579fae98f8d951d05ed426c
lisonge <i@songe.li>
2022-10-24 10:01:49 +0800

    perf: a tag rel="noreferrer"

--- packages/create-monkey/template-svelte-ts/src/App.svelte
+++ packages/create-monkey/template-svelte-ts/src/App.svelte
@@ -6,10 +6,10 @@
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank"> 
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src={viteLogo} class="logo" alt="Vite Logo" />
     </a>
-    <a href="https://svelte.dev" target="_blank"> 
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>

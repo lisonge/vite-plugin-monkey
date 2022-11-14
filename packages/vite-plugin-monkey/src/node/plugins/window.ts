a393287ff9877584e4a665203b3ffc36d04cf6b8
lisonge <i@songe.li>
2022-11-14 21:04:18 +0800

    fix: use define __MONKEY_WINDOW__

--- packages/vite-plugin-monkey/src/node/plugins/window.ts
+++ packages/vite-plugin-monkey/src/node/plugins/window.ts
@@ -1,24 +1,16 @@
-import { normalizePath, PluginOption } from 'vite';
+import type { PluginOption } from 'vite';
 import type { FinalMonkeyOption } from '../types';
 
 export default (finalPluginOption: FinalMonkeyOption): PluginOption => {
-  let isServe = false;
   return {
     name: 'monkey:window',
     config(_, { command }) {
-      isServe = command == 'serve';
-    },
-    transform(code, id) {
-      id = normalizePath(id);
-      if (id.endsWith('vite-plugin-monkey/dist/client/index.mjs')) {
-        return {
-          code: code.replaceAll(
-            /__MONKEY_WINDOW__/g,
-            isServe ? `document.__monkeyWindow` : `window`,
-          ),
-          map: null,
-        };
-      }
+      return {
+        define: {
+          __MONKEY_WINDOW__:
+            command == 'serve' ? `document.__monkeyWindow` : `window`,
+        },
+      };
     },
   };
 };

93e33b8027e5b0000394ca262d383ee0dd795191
lisonge <i@songe.li>
2022-12-23 14:50:59 +0800

    chore: mini-css demo

---
+++ playground/mini-css/dist/mini-css.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name       mini-css
+// @namespace  npm/vite-plugin-monkey
+// @version    0.0.0
+// @author     monkey
+// @icon       https://vitejs.dev/logo.svg
+// @match      https://i.songe.li/*
+// ==/UserScript==
+
+(o=>{const e=document.createElement("style");e.dataset.source="vite-plugin-monkey",e.innerText=o,document.head.appendChild(e)})(".read-the-docs{color:#888;position:fixed;top:60px;right:0;bottom:0;left:260px;background:#fff;z-index:1050;padding:10px 20px 200px;overflow-y:auto;box-shadow:-2px 2px 6px #bbb}");
+
+(function() {
+  "use strict";
+  const style = "";
+})();

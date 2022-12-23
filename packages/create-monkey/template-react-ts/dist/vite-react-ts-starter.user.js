2db294c01461a7134aa675d5a88bdabad18cd835
lisonge <i@songe.li>
2022-12-23 15:13:39 +0800

    chore: create-monkey@1.23.0

--- packages/create-monkey/template-react-ts/dist/vite-react-ts-starter.user.js
+++ packages/create-monkey/template-react-ts/dist/vite-react-ts-starter.user.js
@@ -13,8 +13,25 @@
 
 (function(require$$0, require$$0$1) {
   "use strict";
-  var jsxRuntime = { exports: {} };
+  var jsxRuntimeExports = {};
+  var jsxRuntime = {
+    get exports() {
+      return jsxRuntimeExports;
+    },
+    set exports(v) {
+      jsxRuntimeExports = v;
+    }
+  };
   var reactJsxRuntime_production_min = {};
+  /**
+   * @license React
+   * react-jsx-runtime.production.min.js
+   *
+   * Copyright (c) Facebook, Inc. and its affiliates.
+   *
+   * This source code is licensed under the MIT license found in the
+   * LICENSE file in the root directory of this source tree.
+   */
   var f = require$$0, k = Symbol.for("react.element"), l = Symbol.for("react.fragment"), m$1 = Object.prototype.hasOwnProperty, n = f.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner, p = { key: true, ref: true, __self: true, __source: true };
   function q(c, a, g) {
     var b, d = {}, e = null, h = null;
@@ -36,8 +53,8 @@
       module.exports = reactJsxRuntime_production_min;
     }
   })(jsxRuntime);
-  const jsx = jsxRuntime.exports.jsx;
-  const jsxs = jsxRuntime.exports.jsxs;
+  const jsx = jsxRuntimeExports.jsx;
+  const jsxs = jsxRuntimeExports.jsxs;
   var client = {};
   var m = require$$0$1;
   {

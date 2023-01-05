8872a3685961dc3755c09ac8058c9cced0951468
lisonge <i@songe.li>
2023-01-05 15:53:48 +0800

    chore: test github.com csp

--- playground/react-ts/vite.config.ts
+++ playground/react-ts/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       userscript: {
         icon: 'https://vitejs.dev/logo.svg',
         namespace: 'npm/vite-plugin-monkey',
-        match: ['https://www.google.com/'],
+        match: ['https://www.google.com/', 'https://github.com/*'],
       },
       build: {
         externalGlobals: {

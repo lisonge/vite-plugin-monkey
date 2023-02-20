ba1b4aabe6a84fcc5534b85223b58ba11b9ece29
lisonge <i@songe.li>
2023-02-20 20:13:44 +0800

    fix: svg logo url

--- packages/create-monkey/template-vue-ts/src/App.vue
+++ packages/create-monkey/template-vue-ts/src/App.vue
@@ -2,15 +2,17 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue';
+import viteLogoUrl from './assets/vite.svg';
+import vueLogoUrl from './assets/vue.svg';
 </script>
 
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
+      <img :src="viteLogoUrl" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img :src="vueLogoUrl" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />

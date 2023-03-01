7616f8769ac2dfc5e1b53551f53496ce9f8fc086
lisonge <i@songe.li>
2023-03-01 21:25:44 +0800

    fix: use pnpm publish

--- scripts/releaseUtils.ts
+++ scripts/releaseUtils.ts
@@ -86,7 +86,7 @@ export async function publishPackage(
   if (tag) {
     publicArgs.push(`--tag`, tag);
   }
-  await runIfNotDry('npm', publicArgs, {
+  await runIfNotDry('pnpm', publicArgs, {
     cwd: pkdDir,
   });
 }

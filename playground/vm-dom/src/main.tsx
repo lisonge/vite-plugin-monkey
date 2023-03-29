73d6db261ec12f3e8aac595aed342d72ca3c2a9b
lisonge <i@songe.li>
2023-03-29 12:07:48 +0800

    chore: vm-dom example

---
+++ playground/vm-dom/src/main.tsx
@@ -0,0 +1,10 @@
+function Greetings() {
+  return (
+    <>
+      <div>hello</div>
+      <p>This is a panel. You can drag to move it.</p>
+    </>
+  );
+}
+
+document.body.appendChild(VM.m(<Greetings />));

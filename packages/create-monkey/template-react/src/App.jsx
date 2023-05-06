5dfc8e84b3586ab5170fa764a3136564bf05e2cb
Coaixy <coaixy@outlook.com>
2023-05-06 19:56:33 +0800

    replace 'vite' with 'react'

--- packages/create-monkey/template-react/src/App.jsx
+++ packages/create-monkey/template-react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
 import './App.css';
 
 function App() {

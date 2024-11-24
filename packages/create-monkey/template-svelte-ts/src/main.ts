330f8a0876844dc849a3708aa758926209591cff
Lucas Shanley <lpshanley@Lucass-MacBook-Pro.local>
2024-11-23 21:39:29 -0600

    Upgrade svelte-ts to version 5
    Overview
    This upgrades template-svelte-ts to version 5 and aligns the rest
    of the changes to the latest from the svelte package.

--- packages/create-monkey/template-svelte-ts/src/main.ts
+++ packages/create-monkey/template-svelte-ts/src/main.ts
@@ -1,12 +1,13 @@
+import { mount } from 'svelte';
 import './app.css';
 import App from './App.svelte';
 
-const app = new App({
+const app = mount(App, {
   target: (() => {
     const app = document.createElement('div');
     document.body.append(app);
     return app;
-  })(),
-});
+  })()
+})
 
 export default app;

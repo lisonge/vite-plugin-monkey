90203818d608270adb94572768c0ecdc59d78478
二刺螈 <i@songe.li>
2024-12-14 22:02:12 +0800

    chore: test userscript meta tag

--- playground/mini-css/dist/mini-css.user.js
+++ playground/mini-css/dist/mini-css.user.js
@@ -5,6 +5,8 @@
 // @author     author_name
 // @icon       https://vitejs.dev/logo.svg
 // @match      https://songe.li/*
+// @tag        233
+// @tag        456
 // @grant      GM_addStyle
 // @grant      GM_getValue
 // @grant      GM_registerMenuCommand

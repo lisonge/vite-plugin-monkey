e80613aca784cf3ac0921595a74b7538d9c5cde4
二刺螈 <i@songe.li>
2024-12-21 15:08:57 +0800

    fix: GmSetClipboardType type (#204)

--- packages/vite-plugin-monkey/src/client/types/setClipboard.ts
+++ packages/vite-plugin-monkey/src/client/types/setClipboard.ts
@@ -1,7 +1,7 @@
 export interface GmSetClipboardType {
-  (data: string, type: string): void;
+  (data: string, type: string, cb?: () => void): void;
 }
 
 export interface GmAsyncSetClipboardType {
-  (data: string, type: string): Promise<void> | void;
+  (data: string, type: string, cb?: () => void): Promise<void> | void;
 }
